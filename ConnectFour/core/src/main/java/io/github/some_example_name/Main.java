70ca82cf391c1c0cfa2c1cfe1ded18f9ced22163
Vincentc233 <VIncentc270@nycstudents.net>
2025-05-27 00:04:06 -0400

    Update Main.java

--- ConnectFour/core/src/main/java/io/github/some_example_name/Main.java
+++ ConnectFour/core/src/main/java/io/github/some_example_name/Main.java
@@ -13,63 +13,85 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener{
-    Vector2 touchPos; // By creating a single instance variable rather than a local variable we can prevent the garbage collector from triggering, which prevents lag spikes
-
+    Vector2 touchPos = new Vector2(); // By creating a single instance variable rather than a local variable we can prevent the garbage collector from triggering, which prevents lag spikes
     Texture backgroundTexture;
-    Texture blueCoinTexture;
-    Sprite blueCoin;
+    Texture yellowCoinTexture;
+    Sprite yellowCoin;
     Texture redCoinTexture;
     Sprite redCoin;
     SpriteBatch spriteBatch;
     FitViewport viewport;
-
-    @Override //overrides its superclass method
+    int redScore = 0;
+    int yellowScore = 0;
+    private float inputCooldown = 0f;
+    private static final float COOLDOWN_TIME = 0.3f;
+    public int[][] gameBoard = new int[6][7];
+    private boolean redTurn = true;
     public void create() {
-        viewport = new FitViewport(8, 5);
-        touchPos = new Vector2();
-        backgroundTexture = new Texture("background.png");
-        blueCoinTexture = new Texture("blue.png");
-        redCoinTexture = new Texture("red.png");
-        blueCoin = new Sprite(blueCoinTexture);
+
+        viewport = new FitViewport(7, 6);
+        backgroundTexture = new Texture("board.png");
+        yellowCoinTexture = new Texture("YellowCoin.png");
+        redCoinTexture = new Texture("RedCoin.png");
+        yellowCoin = new Sprite(yellowCoinTexture);
         redCoin = new Sprite(redCoinTexture);
-        redCoin.setSize(1,1);
-        blueCoin.setSize(1,1);
+        redCoin.setSize(1.5f,1.5f);
+        yellowCoin.setSize(1.5f,1.5f);
         spriteBatch = new SpriteBatch();
-
-
     }
 
     @Override
     public void render() {
-        input();
-        logic();
+        float deltaTime = Gdx.graphics.getDeltaTime(); // Time since last frame
+        if (inputCooldown > 0) {
+            inputCooldown -= deltaTime;
+        }
+        play();
         draw();
+        updateScore();
     }
-
 
     private void input() {
-        float speed = 5;
-        float delta = Gdx.graphics.getDeltaTime(); // returns time between frames(delta)
 
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            blueCoin.translateX(speed*delta); // Moves the sprite
-        }
-        else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            blueCoin.translateX(-speed * delta);
-        }
-
-        if(Gdx.input.isTouched()){ // If user clicks the screen
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // gets where the user clicked
-            viewport.unproject(touchPos); // Covert the U
-            blueCoin.setCenterX(touchPos.x);
-        }
     }
 
-    private void logic() {
+    private void play(){
+        if (Gdx.input.isTouched() && inputCooldown <= 0){
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // gets where the user clicked
+            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
+            for (int c = 0; c < 7; c++) {
+                if (touchPos.x >= c && touchPos.x < c+1) {
+                    for (int r = 5; r > -1; r--) {
+                        if (gameBoard[r][c] == 0) {
+                            if(redTurn) {
+                                gameBoard[r][c] = 2; //sets it to a redCoin
+                                redTurn = false;
 
+                            }
+                            else {
+                                gameBoard[r][c] = 1; //sets it to a blueCoin
+                                redTurn = true;
+
+                            }
+                            inputCooldown = COOLDOWN_TIME;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+            System.out.println(touchPos.x);
+        }
+
+    }
+    private void logic() {
+        float worldWidth = viewport.getWorldWidth();
+        float worldLength = viewport.getWorldHeight();
+
+        float coinWidth = yellowCoin.getWidth();
+        float coinHeight = yellowCoin.getHeight();
     }
 
     private void draw() {
@@ -80,16 +102,77 @@ public class Main implements ApplicationListener{
 
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
-
-        blueCoin.draw(spriteBatch);
-        redCoin.draw(spriteBatch);
-
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        for(int r =0 ; r<6; r++){
+            for(int c =0;c<7; c++){
+                if(gameBoard[r][c] ==0) continue;
+                else if(gameBoard[r][c] == 2){
+                    redCoin.setX(c);
+                    redCoin.setY(5-r);
+                    redCoin.draw(spriteBatch);
+                }
+                else{
+                    yellowCoin.setX(c);
+                    yellowCoin.setY(5-r);
+                    yellowCoin.draw(spriteBatch);
+                }
+            }
+        }
         spriteBatch.end();
     }
 
+    private void updateScore() {
+        boolean allEqual = true;
+        for (int r = 0; r < gameBoard.length; r++) {
+            for (int c = 0; c < gameBoard[0].length; c++) {
+                if (gameBoard[r][c] != 0 && c < gameBoard[0].length-4) {//checks for horizontal connect 4s
+                    for (int i = 1; i <= 3; i++) {
+                        if (gameBoard[r][c] == gameBoard[r][c + i]) {
+                            continue;
+                        }
+                        else allEqual = false;
+                    }
+                    if (allEqual && gameBoard[r][c] == 2){
+                        redScore++;
+                        System.out.println("RedScore:" + redScore);
+                    }
+                    else if (allEqual && gameBoard[r][c] == 1){
+                        yellowScore++;
+                        System.out.println("YellowScore:" + yellowScore);
+                    }
+                    allEqual = true;
+                    break;
+                }
+                else if (gameBoard[r][c] != 0 && r < gameBoard.length-4) {//checks for vertical connect 4s
+                    for (int i = 1; i <= 3; i++) {
+                        if (gameBoard[r][c] == gameBoard[r+i][c]) {
+                            continue;
+                        }
+                        else allEqual = false;
+                    }
+                    if (allEqual && gameBoard[r][c] == 2){
+                        redScore++;
+                        System.out.println("RedScore:" + redScore);
+                    }
+                    else if (allEqual && gameBoard[r][c] == 1){
+                        yellowScore++;
+                        System.out.println("YellowScore:" + yellowScore);
+                    }
+                    allEqual = true;
+                    break;
+                }
+            }
+        }
+    }
+
+
+
     @Override
     public void dispose() {
-
+        backgroundTexture.dispose();
+        redCoinTexture.dispose();
+        yellowCoinTexture.dispose();
+        spriteBatch.dispose();
     }
 
     @Override
@@ -107,3 +190,4 @@ public class Main implements ApplicationListener{
 
     }
 }
+
